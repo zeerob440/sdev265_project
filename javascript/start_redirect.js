1adb676f6ab77fe7736c38571cdc6937cda67967
MU-TH-ER <zeerob440@tutanota.com>
2026-04-02 13:44:19 -0400

    experiment: index.html| declared directory 'javascript' declared button on index.html, expect to use star_redirect.js to redirect user from index.html, to bus-stop.html.

---
+++ javascript/start_redirect.js
@@ -0,0 +1,7 @@
+// start_redirect is a short script that uses an event listener to redirect the user to bus_stop.html
+
+document.getElementById('pressToStart').addEventListener('click', titleToBus);
+
+function titleToBus()
+    {window.location.href ="../bus-stop.html";
+    }
