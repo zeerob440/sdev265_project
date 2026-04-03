0f31f1e6c6aa83d1c2a6d5ce520d4e01ea0cd408
MU-TH-ER <zeerob440@tutanota.com>
2026-04-03 09:39:10 -0400

    update: start_redirect.js| documentation improved.

--- javascript/start_redirect.js
+++ javascript/start_redirect.js
@@ -1,7 +1,13 @@
-// start_redirect is a short script that uses an event listener to redirect the user to bus_stop.html
+/*
+    start_redirect.js serves as the central navigation program for all user navigation
+    decisions in Time Shift. This program contains event listeners linked to HTML pages
+    and functions that redirect the user based on their decisions in the game.  
+*/
 
+// Event listener for start to bus-stop (index.html-->bus-stop.html)
 document.getElementById('pressToStart').addEventListener('click', titleToBus);
 
+//function for start to bus-stop (index.html-->bus-stop.html)
 function titleToBus(){
     window.location.href ="bus-stop.html";
     }
