80c939f03bdc4b0c4ab517f1655f598bdccc3550
zeerob440 <zeerob440@tutanota.com>
2026-04-04 14:40:00 -0400

    feat: start.redirect.js| linked game-over.html to index.html

--- javascript/start_redirect.js
+++ javascript/start_redirect.js
@@ -17,18 +17,28 @@ const gloves_button = document.getElementById('gloves');
 function busToFuture(){
     window.location.href ="corte-real/css-corte-real.html";
 }
+
 // var for css-corte-real.html to frame_d.html
 const frame_d_button = document.getElementById('frame_d_nav');
 // function for css-corte-real.html to frame_d.html
 function futureToFrameD(){
     window.location.href = "../frame_d/frame_d.html";
 }
+
 // var for frame_d.html to game-over.html
 const frame_d_game_over_proceed_button = document.getElementById('frame_d_gameover');
 // function for frame_d.html to game-over.html
 function frameDToGameOver() {
-    window.location.href = "../game-over/game-over.html"
+    window.location.href = "../game-over/game-over.html";
 }
+
+//var for game-over.html to index.html
+const exit_to_main_button = document.getElementById('exit_main_nav');
+//function for game-over.html to index.html
+function gameOverToMain(){
+    window.location.href = "../index.html";
+}
+
 
 
 // if-if structure to determine which function to invoke from html pages via event listeners.
@@ -48,4 +58,8 @@ if (frame_d_button) {
 // Event listener for frame_d.html to game-over.html
 if (frame_d_game_over_proceed_button){
     frame_d_game_over_proceed_button.addEventListener('click', frameDToGameOver);
+}
+//event listener for game-over.html to index.html
+if (exit_to_main_button){
+    exit_to_main_button.addEventListener('click', gameOverToMain);
 }
