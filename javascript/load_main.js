d27bfdd20266a0238ae5c5fb63b193a62adeb402
zeerob440 <zeerob440@tutanota.com>
2026-04-04 16:33:58 -0400

    debug: index.html, play music, removed nav button form innerHTML

--- javascript/load_main.js
+++ javascript/load_main.js
@@ -4,14 +4,15 @@
 const menu = document.getElementById('time_to_time_travel');
 
 function printMe()
+
+const music =document.getElementById('theme');
+theme.play()
+
 {document.getElementById('menuLoadArea').innerHTML = `
     <h1>This is Time Shift</h1>
 		<p> 
 			The date is March 25th 2026, but time does not flow linearly. Can you navigate space and time to return to your timeline?
-		</p>
-
-		<button id="pressToStart">START</button>`; 
-
+		</p>`;
 }
 if (menu){
     menu.addEventListener('click', printMe);
