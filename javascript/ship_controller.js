d41631274c3e13a4e631748867838284f5d0f2dc
zeerob440 <zeerob440@tutanota.com>
2026-04-05 13:48:18 -0400

    feat: frame_c.html filling out story

--- javascript/ship_controller.js
+++ javascript/ship_controller.js
@@ -1,6 +1,6 @@
 // ship_controller.js controls the UI output on inside the ship_controller class from future.css
 
-/* 
+/* notes form frame_c.html to make writing js function target easier.
     <button id="om_astral_nav">Astral Nav</button>
     <button id="om_armory">Armory</button>
     <button id="om_propulsion">Propulsion</button>
@@ -11,19 +11,30 @@
     <div class="propulsion_output"></div>
  </div>
 */
-
+// var to target button frame_c.html, om_atmosphere_vent
 const om_atmosphere_vent_button = document.getElementById('om_atmosphere_vent');
+// function to print innerHTML when om_atmosphere_vent is interacted with by user.
+function ventAtmos(){
+    document.getElementById('atmosphere_output').innerHTML = `<p>LIFE SAFETY VIOLATION: o2 cannot be reduced to below 19.5% while ship is occupied!</p>`;
+   }
 
-function ventAtmos(){document.getElementById('atmosphere_output').innerHTML = `<p>LIFE SAFETY VIOLATION: o2 cannot be reduced to below 19.5% while ship is occupied!</p>`;
-    
-}
+// var to target button frame_c.html, om_armory   
 const om_armory_button = document.getElementById('om_armory');
+
+function armoryStatus(){
+    document.getElementById('armory_output').innerHTML =`<p>01 MK-540 Electro-Magnetic Carbine, with 450 depleted U92 munitions operational.
+    Other armaments depleted from attempted apprehension of UNREGISTERED PASSENGER 01 in 2322.</p>`}
 
 const om_propulsion_button = document.getElementById('om_propulsion');
 
 const om_astral_nav_button = document.getElementById('om_astral_nav');
 
+//if if structure to allow this program to be used on multiple pages.
 
+// event listener for frame_c.html om_atmosphere_vent.
 if (om_atmosphere_vent_button){
     om_atmosphere_vent_button.addEventListener('click', ventAtmos);
+}
+if (om_armory_button){
+    om_armory_button.addEventListener('click', armoryStatus);
 }
