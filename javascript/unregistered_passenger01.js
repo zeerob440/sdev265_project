8670ffb67c86cee9d95860aefd5b1e9166c776f9
zeerob440 <zeerob440@tutanota.com>
2026-04-14 18:29:35 -0400

    feat: css-corte-real.html, armory2.html| linked unregistered_passenger01.js

--- javascript/unregistered_passenger01.js
+++ javascript/unregistered_passenger01.js
@@ -2,5 +2,24 @@
 
 const roar =Audio("../music/the_creature.mp3");
 
+let min_milsec = 14000;
+
+let max_milsec = 33000;
 
 roar.play();
+
+function randomRoar(){
+    let roar_interval = Math.floor(Math.random() * (max_milsec - min_milsec +1))
+    + min_milsec;
+
+    setTimeout(() => {
+        roar.currentTime = 0;
+        roar.play;
+
+        randomRoar();
+    }, roar_interval);
+
+}
+randomRoar();
+
+if (window)randomRoar();
