a394728f60099a73ce3e3a18753af2c5c3ae3369
zeerob440 <zeerob440@tutanota.com>
2026-04-14 19:26:47 -0400

    debug:  unregistered_passenger01.js| reduce roar frequency

--- javascript/unregistered_passenger01.js
+++ javascript/unregistered_passenger01.js
@@ -4,9 +4,9 @@ const roar = new Audio("../music/the_creature.mp3");
 
 let min_milsec = 14000;
 
-let max_milsec = 33000;
+let max_milsec = 45000;
 
-roar.play();
+//roar.play();
 
 function randomRoar(){
     let roar_interval = Math.floor(Math.random() * (max_milsec - min_milsec +1))
@@ -15,11 +15,9 @@ function randomRoar(){
     setTimeout(() => {
         roar.currentTime = 0;
         roar.play();
-
         randomRoar();
     }, roar_interval);
 
-}
-randomRoar();
+};
 
 if (document)randomRoar();
