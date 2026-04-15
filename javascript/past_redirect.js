2b4adfbcb41f7100e8a40218211f0f4e1824a82a
zeerob440 <zeerob440@tutanota.com>
2026-04-15 18:54:48 -0400

    fixed: past_redirect.js| filepath error from bus-stop.html to Encounter1.html

--- javascript/past_redirect.js
+++ javascript/past_redirect.js
@@ -2,7 +2,7 @@
 const hat_button =document.getElementById('hat');
 // function for bus-stop.html to Encounter1.html
 function busToPast(){
-    window.location.href='Encounter/Encounter1.html';
+    window.location.href='../Encounter/Encounter1.html';
     console.log('Redirecting from bus-stop.html to Encounter1.html');
 }
 
