b8fe86a6e0240c189f94ec5106c9b7cfbbac5755
MU-TH-ER <zeerob440@tutanota.com>
2026-04-22 15:05:15 -0400

    debug: no_go_back.js| game over on refresh functions as expected. Experiment with redirect to game over on back

--- javascript/no_go_back.js
+++ javascript/no_go_back.js
@@ -1,7 +1,19 @@
 //no_go_back.js redirects user to game_over.html if back or refresh is used in game
 
+// Redirects back to game-over.html
+function noBack(event){
+    if (event.persisted) {
+        window.location.href='/sdev265_project/game-over/game-over.html';
+        console.log('Time paradox. One does not simply "go back" in time!');
+    }
+}
+
+window.addEventListener('pageshow', noBack);
+
+//redirects refresh to game-over.html
 if(document){};
     if(performance.navigation.type !== 0){
-        //window.location.href='../game-over/game-over.html';
         window.location.href='/sdev265_project/game-over/game-over.html';
+        console.log('Time Paradox deja vu means there is a glitch in The Matrix!');
     }
+    
