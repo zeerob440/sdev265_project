e75d30b5ccae65b9e273bfc09629fa0e69722e0f
zeerob440 <zeerob440@tutanota.com>
2026-04-23 17:29:40 -0400

    update: gameover_message.js| added more quotes

--- javascript/gameover_message.js
+++ javascript/gameover_message.js
@@ -1,12 +1,12 @@
 // prints a random message to innerHtml on game-over.html
 
 const min = 0;
-const max = 1;
+const max = 3;
 
 function randomMessage(){ let value = Math.floor(Math.random() * (max - min +1)) + min;
 
     let messages =['Existence is an ephemeral flower that grows in a cosmically indifferent universe...',
-    'Time will continue, always.'
+    'Time will continue, always...','All that is weak is destroyed by time...','The universe only answers in silence...'
 ];
     
     document.getElementById('go_message').innerHTML = messages[value];
